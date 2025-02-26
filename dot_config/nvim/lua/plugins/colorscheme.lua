22966022a4fbb2e15d256e0c730ef8239986be5c
Russell Teabeault <rteabeault@gmail.com>
2025-02-25 23:51:39 -0700

    nvim color scheme

---
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    --- "catppuccin/nvim",
+    --- "marko-cerovac/material.nvim",
+    "rebelot/kanagawa.nvim",
+    opts = {
+      --- background = { -- :h background
+      ---   dark = "mocha",
+      --- },
+    },
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      --- colorscheme = "material-darker",
+      colorscheme = "kanagawa-dragon",
+    },
+  },
+}
